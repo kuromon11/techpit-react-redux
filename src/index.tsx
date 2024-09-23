9b72c037f1387960f8a513a79a9ca07105753273
kuromon11 <yoshikikuro712@gmail.com>
2024-09-23 23:48:22 +0900

    feat: #2 カンバンボードの最低限の見た目部分を作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import styled from 'styled-components';
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <Container>
+      <Header>
+        <Logo>Kanban board</Logo>
+
+        <CardFilter placeholder="Filter cards" />
+      </Header>
+
+      <MainArea>
+        <Column>
+          <ColumnHeader>TODO</ColumnHeader>
+
+          <Card>朝食をとる🍞</Card>
+          <Card>SNSをチェックする🐦</Card>
+          <Card>布団に入る (:3[___]</Card>
+        </Column>
+
+        <Column>
+          <ColumnHeader>Doing</ColumnHeader>
+
+          <Card>顔を洗う👐</Card>
+          <Card>歯を磨く🦷</Card>
+        </Column>
+
+        <Column>
+          <ColumnHeader>Waiting</ColumnHeader>
+        </Column>
+
+        <Column>
+          <ColumnHeader>Done</ColumnHeader>
+
+          <Card>布団から出る (:3っ)っ -=三[＿＿]</Card>
+        </Column>
+      </MainArea>
+    </Container>
+  );
 }
+
+const Container = styled.div``;
+
+const Header = styled.div``;
+
+const Logo = styled.div``;
+
+const CardFilter = styled.input``;
+
+const MainArea = styled.div``;
+
+const Column = styled.div``;
+
+const ColumnHeader = styled.div``;
+
+const Card = styled.div``;
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
