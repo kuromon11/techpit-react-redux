8feb679dac90897d8491880be5c8649ea5e0db3b
kuromon11 <yoshikikuro712@gmail.com>
2024-10-05 15:00:51 +0900

    feat: #3 Cardのフィルタリング機能を実装

--- src/CardFilter.tsx
+++ src/CardFilter.tsx
@@ -5,11 +5,21 @@ import * as color from './color';
 
 import { SearchIcon as _SearchIcon } from './icon';
 
-export function CardFilter() {
+export function CardFilter({
+  value,
+  onChange,
+}: {
+  value?: string;
+  onChange?(value: string): void;
+}) {
   return (
     <Container>
       <SearchIcon />
-      <Input placeholder="Filter cards" />
+      <Input
+        placeholder="Filter cards"
+        value={value}
+        onChange={(ev) => onChange?.(ev.currentTarget.value)}
+      />
     </Container>
   );
 }
