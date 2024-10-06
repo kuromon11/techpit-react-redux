61e2c79c72abeca9ee8d03c9a2c308019ca79a5b
kuromon11 <yoshikikuro712@gmail.com>
2024-10-06 12:13:38 +0900

    feat: #3 Cardの並び替えを実装

--- src/Column.tsx
+++ src/Column.tsx
@@ -12,6 +12,8 @@ export function Column({
   title,
   filterValue: rawFilterValue,
   cards: rawCards,
+  onCardDragStart,
+  onCardDrop,
 }: {
   title?: string;
   filterValue?: string;
@@ -19,6 +21,8 @@ export function Column({
     id: string;
     text?: string;
   }[];
+  onCardDragStart?(id: string): void;
+  onCardDrop?(entered: string | null): void;
 }) {
   const filterValue = rawFilterValue?.trim();
   const keywords = filterValue?.toLowerCase().split(/\s+/g) ?? [];
@@ -34,6 +38,10 @@ export function Column({
   const cancelInput = () => setInputMode(false);
   // ドラッグオーバー（動かしていない場合はエフェクトを表示しない）
   const [draggingCardID, setDraggingCardID] = useState<string | undefined>(undefined);
+  const handleCardDragStart = (id: string) => {
+    setDraggingCardID(id);
+    onCardDragStart?.(id);
+  };
 
   return (
     <Container>
@@ -63,10 +71,11 @@ export function Column({
               draggingCardID !== undefined &&
               (id === draggingCardID || cards[i - 1]?.id === draggingCardID)
             }
+            onDrop={() => onCardDrop?.(id)}
           >
             <Card
               text={text}
-              onDragStart={() => setDraggingCardID(id)}
+              onDragStart={() => handleCardDragStart(id)}
               onDragEnd={() => setDraggingCardID(undefined)}
             />
           </Card.DropArea>
@@ -74,6 +83,7 @@ export function Column({
         <Card.DropArea
           style={{ height: '100%' }}
           disabled={draggingCardID !== undefined && cards[cards.length - 1]?.id === draggingCardID}
+          onDrop={() => onCardDrop?.(null)}
         />
       </VerticalScroll>
     </Container>
